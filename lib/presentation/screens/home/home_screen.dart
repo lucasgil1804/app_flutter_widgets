bd946b3cd6dcd2628f5e33b49cf14bf2b8957ecb
lucasgilTelco <94456075+lucasgilTelco@users.noreply.github.com>
2025-04-17 11:36:18 -0300

    Go router configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/home/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -52,8 +53,7 @@ class _CustomListTile extends StatelessWidget {
       ),
       trailing: Icon(Icons.arrow_forward_ios_rounded),
       onTap: () {
-        Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const ButtonsScreen()));
+        context.push(menuItems.link);
       },
     );
   }
